ca13f47bac8e9613837d80990b8a8e9e259b9813
MihailKurakin <wallsoftheb77@gmail.com>
2021-02-17 13:26:30 +0300

    added configuration for deploy on web-server (war)

--- src/main/java/com/movierating/server/MainMovieRating.java
+++ src/main/java/com/movierating/server/MainMovieRating.java
@@ -2,13 +2,15 @@ package com.movierating.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
-public class MainMovieRating {
+public class MainMovieRating extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MainMovieRating.class, args);
@@ -22,5 +24,9 @@ public class MainMovieRating {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainMovieRating.class);
     }
 }
